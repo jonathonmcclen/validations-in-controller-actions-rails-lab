f85569e539664f1ef89409cb1ebaee977454a42b
--replace-all <jonathonsmcclendon@gmail.com>
2021-11-28 12:56:25 -0600

    commit tree c0604474e9d9acca8dbc500fdb4bb34ba3bab08d

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    #the content is at least 100 characters long.
+    validates :content, length: {minimum: 100}
+    validates :category, inclusion: { in: %w(Fiction  Non-Fiction),
+        message: "%{value} is not a valid size" }
 end
